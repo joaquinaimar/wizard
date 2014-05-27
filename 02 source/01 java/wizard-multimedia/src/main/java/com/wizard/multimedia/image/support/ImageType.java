b07a8cb9c5b8f22dab1d84fc7625643071dbe92a
lizhi <joaquinaimar@163.com>
2014-05-27 09:37:41 +0800

    去除警告
    Signed-off-by: lizhi <joaquinaimar@163.com>

--- 02 source/01 java/wizard-multimedia/src/main/java/com/wizard/multimedia/image/support/ImageType.java
+++ 02 source/01 java/wizard-multimedia/src/main/java/com/wizard/multimedia/image/support/ImageType.java
@@ -1,13 +1,9 @@
 package com.wizard.multimedia.image.support;
 
-import java.awt.image.BufferedImage;
-import java.io.FileOutputStream;
-
 public interface ImageType {
 
 	static final int TYPE_JPEG = 0;
 
 	static final int TYPE_TIFF = 1;
-
 
 }
